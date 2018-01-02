cf64728a277cb84e9908d7d1acad683150196da7
Pawel Urbanski <pawel@clusterone.ca>
2018-01-01 17:17:17 -0700

    test

---
+++ ElatePortal/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ElatePortal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
