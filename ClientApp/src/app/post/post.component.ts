4ae2739355a943a6b1b2833d12cf767d039167f7
clayhenry <pawel@localhost.localdomain>
2018-07-07 10:55:55 -0600

    line clenup

--- ClientApp/src/app/post/post.component.ts
+++ ClientApp/src/app/post/post.component.ts
@@ -21,7 +21,6 @@ export class PostComponent implements OnChanges, OnInit {
 
   }
 
-
   ngOnChanges(changes: SimpleChanges) {
     let postid = changes.postid;
     this.postid = postid.currentValue;
@@ -29,7 +28,6 @@ export class PostComponent implements OnChanges, OnInit {
     if (postid.currentValue) {
       this.getBlogPost(postid.currentValue)
     }
-
 
   }
 
