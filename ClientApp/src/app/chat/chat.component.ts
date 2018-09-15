406992d6ec0b1b9626787b0db812a4d4506995e8
Pawel Urbanski <pawel@clusterone.ca>
2018-09-15 15:48:51 -0600

    select users from typeahead liet

--- ClientApp/src/app/chat/chat.component.ts
+++ ClientApp/src/app/chat/chat.component.ts
@@ -13,7 +13,9 @@ import {DataService} from "../data.service";
 @Pipe({ name: 'users' })
 export class ChatComponent implements OnInit {
 
-  constructor(private http: HttpClient,  private _data: DataService,) { }
+  constructor(private http: HttpClient,  private _data: DataService,) {
+
+  }
 
 
   chat =[];
@@ -21,7 +23,10 @@ export class ChatComponent implements OnInit {
   users : IUsers;
   filteredList = [];
   currentSearchString = "";
-  currentcharindex = 0;
+  currentChatBox;
+  arrowindex = 0;
+  listElements : HTMLCollectionOf<HTMLElement>;
+  selectedListContent;
 
 
   ngOnInit() {
@@ -29,6 +34,17 @@ export class ChatComponent implements OnInit {
     let connection = new HubConnectionBuilder().withUrl("message").build();
     connection.start().then(()=>console.log("connected"));
     connection.on('send',data =>{ this.chat.push(data)})
+    this.currentChatBox = document.getElementById("message");
+  }
+
+
+  processTabPres(event){
+    if(event.keyCode == 9){
+
+      console.log("tab")
+
+      document.getElementById("message").focus();
+    }
 
   }
 
@@ -46,6 +62,7 @@ export class ChatComponent implements OnInit {
 
   parseKey(event){
     this.filteredList = [];
+
 
     //include only alphanumeric
     var regex = new RegExp("^[a-zA-Z0-9]+$");
@@ -72,6 +89,9 @@ export class ChatComponent implements OnInit {
         if(this.currentSearchString.length == 0){
           this.filteredList = [];
           this.currentSearchString = "";
+          this.currentChatBox.innerHTML = "";
+          console.log(0)
+
         } else {
           this.UpdateUserList()
         }
@@ -79,11 +99,12 @@ export class ChatComponent implements OnInit {
       }
       //spcebar
       if(event.keyCode == 32){
-
+        this.updateArrowKeys(event);
         //end recording
         this.currentSearchString = "";
         this.filteredList = [];
       }
+
     }
   }
 
@@ -101,18 +122,124 @@ export class ChatComponent implements OnInit {
 
           //console.log(searchString + " " + user.indexOf(searchString) + " " +u[i].name);
             this.filteredList.push(u[i].name);
-
         }
-
       }
     })
 
   }
 
-  tabEnter(event){
+//first action
+  tabAction(event){
+
+    if(event.keyCode == 9){
+      this.selectedListContent = this.filteredList[0];
+      this.listElements  = document.querySelectorAll(".nameslist li") as HTMLCollectionOf<HTMLElement>;
+
+      if( this.listElements.length != 0){
+        this.listElements[0].classList.add("active");
+      }
+    }
 
 
   }
+
+
+  //on enter
+  tabEnter(event, content){
+
+    event.preventDefault();
+
+    this.updateArrowKeys(event);
+
+    if(event.key =="Enter"){
+
+      this.updateMessageTextField();
+
+    }
+  }
+
+
+  //arrows up and down
+  updateArrowKeys(event){
+
+    let currentList = this.filteredList;
+    console.log(currentList[1])
+
+    for(let i = 0; i< this.listElements.length; i++){
+      this.listElements[i].classList.remove("active");
+    }
+
+    if(event.key == "ArrowDown"){
+      if(this.arrowindex < (this.listElements.length -1 )){
+
+        this.arrowindex ++;
+        console.log(this.arrowindex)
+      }
+    }
+
+    if(event.key == "ArrowUp"){
+
+      if(this.arrowindex > 0){
+        this.arrowindex --;
+        console.log(this.arrowindex)
+      }
+    }
+
+    if(this.listElements.length > 0){
+
+      console.log(this.listElements.length)
+      this.selectedListContent = this.filteredList[this.arrowindex];
+
+      let index = this.arrowindex ? this.arrowindex : 0;
+
+        this.listElements[index].classList.add("active");
+
+    }
+
+  }
+
+
+
+  updateMessageTextField(){
+
+    let currentSearchString = "@" + this.currentSearchString.trim().toLowerCase();
+
+
+    let newContent = "&nbsp;<span><b>" + this.selectedListContent + "</b></span>&nbsp;";
+
+    let currentChatString = this.currentChatBox.innerHTML;
+    this.currentChatBox.innerHTML = currentChatString.replace(currentSearchString, newContent);
+
+
+    const range = document.createRange();
+    const sel = window.getSelection();
+    range.selectNodeContents(this.currentChatBox);
+    range.collapse(false);
+    sel.removeAllRanges();
+    sel.addRange(range);
+    this.currentChatBox.focus();
+    range.detach(); // optimization
+
+    this.currentSearchString = "";
+    this.filteredList = [];
+  }
+
+  //regular mouse click
+
+  slectClick(value, index){
+
+    this.listElements  = document.querySelectorAll(".nameslist li") as HTMLCollectionOf<HTMLElement>;
+    this.listElements[index].classList.add("active");
+    this.selectedListContent = this.filteredList[index];
+    this.updateMessageTextField()
+  }
+
+  onKeyUp(ev:KeyboardEvent) {
+    // do something meaningful with it
+    console.log(`The user just pressed ${ev.key}!`);
+  }
+
+
 
 }
 
